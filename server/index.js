dc6f59ac09eeeef78dc81a29f9a79b0c9d847e92
superhsu <31141265+superhsu@users.noreply.github.com>
2018-03-17 11:24:12 -0700

    final commit

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const cors = require('cors');
 
 const path = require('path');
 
+require('dotenv').config();
+
 const app = express();
 
 app.use(bodyParser.text());
@@ -20,7 +22,7 @@ app.use(express.static(path.join(__dirname, '/../src/client')));
 
 app.use('/v1/product', router);
 
-const port = 3002;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
